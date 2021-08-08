6460a926293be8db3f0d007aea7e767aa62dd7d8
Shayla Sawchenko <shayla.sawchenko@recursecreative.com>
2021-08-07 17:05:03 -0700

    Initial Commit

---
+++ Werk It/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Werk It
+//
+//  Created by Shayla Sawchenko on 2021-08-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

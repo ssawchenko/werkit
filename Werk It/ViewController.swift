b92b326d9e6f133291c5a6a51b8c404384409b53
Shayla Sawchenko <shayla.sawchenko@recursecreative.com>
2021-08-08 10:48:27 -0700

    Added super basic UI and ViewController

--- Werk It/ViewController.swift
+++ Werk It/ViewController.swift
@@ -7,13 +7,66 @@
 
 import UIKit
 
+class Exercise {
+    let title: String
+
+    init(title: String) {
+        self.title = title
+    }
+}
+
 class ViewController: UIViewController {
+
+    @IBOutlet weak var firstLabel: UILabel!
+
+    @IBOutlet weak var secondLabel: UILabel!
+
+    @IBOutlet weak var thirdLabel: UILabel!
+
+
+    let allCoreExersises: [Exercise] = [
+        Exercise(title: "Dead Bug w/Band"),
+        Exercise(title: "Dead Bug w/Ball"),
+        Exercise(title: "Windshield Wipers")
+    ]
+
+    let allArmExercises: [Exercise] = [
+        Exercise(title: "Incline Pushups"),
+        Exercise(title: "Resistance Band Pulls"),
+        Exercise(title: "Side Planks")
+    ]
+
+    let allLegExercises: [Exercise] = [
+        Exercise(title: "Squats w/Weights"),
+        Exercise(title: "Ham Curls"),
+        Exercise(title: "Split Squats"),
+        Exercise(title: "Lunges w/Weights"),
+        Exercise(title: "Monster Walks"),
+        Exercise(title: "Wall Sits")
+    ]
+
+    var firstExerciseList : [Exercise] = []
+    var secondExerciseList : [Exercise] = []
+    var thirdExerciseList : [Exercise] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func clickedWerkit(_ sender: UIButton) {
+        firstExerciseList = allCoreExersises
+        secondExerciseList = allArmExercises
+        thirdExerciseList = allLegExercises
+
+        firstLabel.text = getAndRemoveFrom(&firstExerciseList).title
+        secondLabel.text = getAndRemoveFrom(&secondExerciseList).title
+        thirdLabel.text = getAndRemoveFrom(&thirdExerciseList).title
     }
 
 
+    private func getAndRemoveFrom(_ excerises: inout [Exercise]) -> Exercise {
+        let index = Int.random(in: 0..<excerises.count)
+        return excerises.remove(at: index)
+    }
 }
 
